43ca376eab4adeac9b50efd835948dd8c61ae628
Cristina Alonso <cristinaalonso889@gmail.com>
2019-08-24 22:47:45 +0200

    mas

---
+++ src/zoo/BaseDatos.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zoo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author crist
+ */
+public class BaseDatos {
+    
+    private static String Driver = "com.mysql.jdbc.Driver";
+    private static String URL = "jdbc:mysql://localhost:3306/ZOO?ussesSSL=false&serverTimezone=UTC";
+    private static String BBDDUser = "root";
+    private static String BBDDPass = "root";
+    Connection con = null;
+    
+    public BaseDatos(){
+    }
+    
+    public Usuario executeGetUsuario(String sql){
+        ResultSet rs = null;
+        PreparedStatement pstm = null;
+        Usuario u = null;
+        try{
+            Class.forName(Driver);
+            con = DriverManager.getConnection(URL,BBDDUser,BBDDPass);
+            pstm = con.prepareStatement(sql);
+            rs = pstm.executeQuery();
+            while(rs.next()){
+                u = new Usuario();
+                u.setNombre(rs.getString("nombre"));
+                u.setApellido(rs.getString("apellido"));
+                u.setEdad(rs.getInt("edad"));
+                u.setRol(rs.getBoolean("rol"));
+                u.setLogin(rs.getString("login"));
+                u.setPassword(rs.getString("password"));
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }catch (ClassNotFoundException ex) {
+                Logger.getLogger(BaseDatos.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        finally{
+            
+            try {
+                pstm.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return u;
+    }
+    
+    public void closeConection() throws SQLException{
+        con.close();
+    }
+    
+}
---
+++ src/zoo/Usuario.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zoo;
+
+/**
+ *
+ * @author crist
+ */
+public class Usuario {
+    private String nombre;
+    private String apellido;
+    private Integer edad;
+    private Boolean rol;
+    private String login;
+    private String password;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public Boolean getRol() {
+        return rol;
+    }
+
+    public void setRol(Boolean rol) {
+        this.rol = rol;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    
+    
+    
+    
+}
